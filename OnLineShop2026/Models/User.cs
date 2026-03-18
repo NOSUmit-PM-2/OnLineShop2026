94b31c030325fa760e5251c76f5d8479356f5cd9
Geor Guzitaev <georguzitaev@gmail.com>
2026-03-18 16:59:34 +0300

    Подзадача 1. Модель пользователя

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ImagePath { get; set; }
+
+        public User(string firstName, string lastName, string email, string imagePath)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            ImagePath = imagePath;
+            Id = Guid.NewGuid();
+        }
+    }
+}

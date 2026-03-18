aaa293418b6b97ff09292c9c6e4a7941ddcbb0bb
Geor Guzitaev <georguzitaev@gmail.com>
2026-03-18 17:57:12 +0300

    Подзадача 2

--- OnLineShop2026/Models/User.cs
+++ OnLineShop2026/Models/User.cs
@@ -3,15 +3,15 @@
     public class User
     {
         public Guid Id { get; }
-        public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FirstName { get; set; }
         public string Email { get; set; }
         public string ImagePath { get; set; }
 
-        public User(string firstName, string lastName, string email, string imagePath)
+        public User( string lastName, string firstName, string email, string imagePath)
         {
-            FirstName = firstName;
             LastName = lastName;
+            FirstName = firstName;
             Email = email;
             ImagePath = imagePath;
             Id = Guid.NewGuid();

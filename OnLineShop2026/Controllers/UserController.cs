9fdfe66e617ed30c2f2bc7c45c8e8278c8606e90
Давид Плиев <davidpliev2006@gmail.com>
2026-03-28 18:29:40 +0300

    Pliev_lesson_2_1 Создана моедль User, Котроллер UserController и отображение пользователя

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        User user = new User("Давид", "Плиев", "example@gmail.com", "/images/user.png");
+        public IActionResult Index()
+        {
+            return View(user);
+        }
+    }
+}

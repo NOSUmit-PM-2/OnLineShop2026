ea5130893a6d97b247dc3e7b85eec6f2168e15c8
Soqyaro <chopik531@gmail.com>
2026-03-29 19:58:17 +0300

    1) сделал форк 2) создал user.cs 3) выполнил 1 задание

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string AvatarImage { get; set; }
+
+        public User(string name, string surname, string email, string avatarImage)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Email = email;
+            AvatarImage = avatarImage;
+        }
+
+    }
+}

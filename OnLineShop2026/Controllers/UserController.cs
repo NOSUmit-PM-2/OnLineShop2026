3476205762a51d0e311acd6f458a79847fb73344
ParzivaloC <bicherahovartur12@mail.ru>
2026-03-29 19:43:56 +0300

    Подзадача 2
    Добавил картинку для профиля
    
    Добавлен контроллер UserController , в котором создается один пользователь и передается во View

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+            
+            var user = new Models.User("Иван", "Иванов", "ivan.ivanov@example.com");
+            return View(user);
+        }
+    }
+}

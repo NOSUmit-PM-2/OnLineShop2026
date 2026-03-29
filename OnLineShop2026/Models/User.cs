947cbf2713bf56657f44d96b2e506ecf82ac567d
Элина Кокоева <e.kokoeva06@mail.ru>
2026-03-29 17:16:35 +0300

    создана модель

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace OnLineShop2026.Models
+{
+	public class User
+	{
+
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string PhotoPath { get; set; }
+
+		public User(string name, string email, string photoPath = "/images/basePhoto.jpg")
+		{
+			Name = name;
+			Email = email;
+			PhotoPath = photoPath;
+		}
+
+		public override string ToString()
+		{
+			return $"Имя - {Name}, почта - {Email}";
+		}
+	}
+}

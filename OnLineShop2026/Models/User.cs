ab5626f48e05201da5f81b7a630c2724f616640f
LeraKhubulova <lera.khubulova.06@mail.ru>
2026-03-29 22:54:35 +0300

    Создана модель для описания пользователя + дефолт картинка

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; } 
+
+
+
+        public User(string name, string surname, string email, string avatar = "/images/Avatar_Heley.png")
+        {
+            Id = Guid.NewGuid();          
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Avatar = avatar;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя - {Name}, Фамилия - {Surname}, почта - {Email}";
+
+        }
+    }
+}

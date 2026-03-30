df26808c9b49addaa885929a9e2dbeafbd9d1d11
NameIvanov <toster.one@yandex.ru>
2026-03-30 22:33:32 +0300

    Ivanov lesson 2.1

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+
+        public User(string nameSurname, string email, string avatar = "images/default_avatar.jpg")
+        {
+            Id = Guid.NewGuid();
+            NameSurname = nameSurname;
+            Email = email;
+            Avatar = avatar;
+        }
+    }
+}

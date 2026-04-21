b23ca7dcefdc174a0b6a1a58560dd9d6130eca89
Мария МД <mahadrum@yandex.ru>
2026-04-21 10:29:40 +0300

    Добавлено меню администратор

---
+++ OnLineShop2026/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Data;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class AdminController : Controller
+    {
+
+       
+        public IActionResult Index(int idUser)
+        {
+            return View();
+        }
+    }
+}

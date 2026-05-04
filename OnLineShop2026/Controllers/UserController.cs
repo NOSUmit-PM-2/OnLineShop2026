b373a37afb2ffe5e85ec371b32801c1ed3c8de8a
AmletixQ <nazarovamridin308@gmail.com>
2026-05-04 17:29:09 +0300

    Вторая подзадача

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Data;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            User user = new User(
+                "Nazarov",
+                "AmletixQ",
+                "amletixq@best.com",
+                "https://avatars.githubusercontent.com/u/98691645?v=4"
+            );
+
+            return View(user);
+        }
+    }
+}
